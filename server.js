3b0dff04b128fcfc1916f345b8f196106d8ac82b
clayton.dickemann <cadickemann@gmail.com>
2015-04-03 23:09:12 -0500

    Updated some files

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var _ = require("lodash");
 var url = require('url');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 app.use(express.static("."));
 app.listen(port);
-console.log("Server started at: " + "http://localhost:".concat(port));
