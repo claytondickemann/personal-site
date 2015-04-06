2187dc985430176cf72fca779ce5d56f86dfa816
clayton.dickemann <cadickemann@gmail.com>
2015-04-06 15:15:47 -0500

    Changed port number to be 80. Note, must run as root for local machine.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var url = require('url');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.use(express.static("."));
 app.listen(port);
 console.log("Server started at: " + "http://localhost:".concat(port));
