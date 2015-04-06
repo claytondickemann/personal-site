ab7f18fb2ff9c7906601d1089a0f92869b41b52d
clayton.dickemann <cadickemann@gmail.com>
2015-04-06 15:12:13 -0500

    Updated site to include loading page along with spinning icon.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-var _ = require("lodash");
 var url = require('url');
 var port = process.env.PORT || 3000;
 app.use(express.static("."));
 app.listen(port);
+console.log("Server started at: " + "http://localhost:".concat(port));
